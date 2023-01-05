df86c0d8813e127c9815e430e0f61462d2c924ac
williamrlix <williamtimotiussigalemba@gmail.com>
2023-01-05 23:16:31 +0800

    navbar sucses

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,60 @@
-import React, { useEffect, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+
 import { Link, useLocation} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import '../App.css';
 
-const Navbar = () => {
-    return(
-      <>
-        <header>
-          <nav>
-            <div className="menu-icon">
-              <i className="fa fa-bars fa-2x" />
-            </div>
-            <div className="logo">
-              LOGO
-            </div>
-            <div className="menu">
-              <ul>
-                <Link to={"#"}><li>Home</li></Link>
-                <Link to={"#"}><li>Detail Page</li></Link>
-                <Link to={"#"}><li>Login</li></Link>
-                <Link to={"#"}><li>Register</li></Link>
-              </ul>
-            </div>
-          </nav>
-        </header>
-      </>
-      
-    )
+import React, { useEffect, useState } from 'react';
+
+function Navbar() {
+  const [navbarStyle, setNavbarStyle] = useState({
+    backgroundColor: 'transparent',
+    transition: 'background-color 0.5s ease'
+  });
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setNavbarStyle({
+          backgroundColor: '#333',
+          transition: 'background-color 0.5s ease'
+        });
+      } else {
+        setNavbarStyle({
+          backgroundColor: 'transparent',
+          transition: 'background-color 0.5s ease'
+        });
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <>
+      <header>
+        <nav style={navbarStyle}>
+          <div className="menu-icon">
+            <i className="fa fa-bars fa-2x" />
+          </div>
+          <div className="logo">
+            LOGO
+          </div>
+          <div className="menu">
+            <ul>
+              <Link to={"#"}><li>Home</li></Link>
+              <Link to={"#"}><li>Detail Page</li></Link>
+              <Link to={"#"}><li>Login</li></Link>
+              <Link to={"#"}><li>Register</li></Link>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </>
+  );
 }
-export default Navbar
+
+export default Navbar;
