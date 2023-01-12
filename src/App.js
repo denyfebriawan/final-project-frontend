7b20d364b8533d5195b10c9cbcdff1099627cfbb
Deny Febriawan <denyfebriawan@gmail.com>
2023-01-12 16:34:05 +0700

    mergePoint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Footer from './components/Footer';
