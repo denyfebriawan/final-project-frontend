ef870985927795ea8e72190903b72c3d5f1531c0
williamrlix <williamtimotiussigalemba@gmail.com>
2023-01-27 12:01:10 +0800

    asd

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,35 @@
-
-import { Link} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.css';
-import '../App.css';
-import React, { useEffect, useState } from 'react';
+import "bootstrap/dist/css/bootstrap.css";
+import "../App.css";
+import React, { useEffect, useState, useCallback } from "react";
+import { Link, useLocation } from "react-router-dom";
+import { logout } from "../redux/actions/auth";
+import { clearMessage } from "../redux/actions/message";
+import { useDispatch, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 
 function Navbar() {
+  const { user: currentUser } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
+
+  const location = useLocation();
+
+  useEffect(() => {
+    if (["/signin", "/signup"].includes(location.pathname)) {
+      dispatch(clearMessage());
+    }
+  }, [dispatch, location]);
+
+  const handleLogout = useCallback(() => {
+    dispatch(logout());
+  }, [dispatch]);
+
   const [navbarStyle, setNavbarStyle] = useState({
     backgroundColor: 'transparent',
     transition: 'background-color 0.5s ease'
   });
 
   useEffect(() => {
-    
     const handleScroll = () => {
       if (window.scrollY > 0) {
         setNavbarStyle({
@@ -33,20 +51,44 @@ function Navbar() {
     };
   }, []);
 
+
+
   return (
     <>
         <nav style={navbarStyle} className="sticky">
-          <div className="fixed-element">
-          </div>
-          <div className="logo">
-            LOGO
-          </div>
+          <div className="fixed-element"></div>
+          <div className="logo">LOGO</div>
           <div className="menu">
             <ul>
-              <Link to={"/"}><li>Home</li></Link>
-              <Link to={"/SubPage"}><li>Destination Page</li></Link>
-              <Link to={"#"}><li>Login</li></Link>
-              <Link to={"#"}><li>Register</li></Link>
+              <Link to={"/"}>
+                <li>Home</li>
+              </Link>
+              <Link to={"Subpage"}>
+                <li>Destination Page</li>
+              </Link>
+
+              {currentUser ? (
+                <>
+                  <Link to={"/signin"} onClick={handleLogout}>
+                    <li>Log Out</li>
+                  </Link>
+                  <Link to={"/"}>
+                    <li>{`${currentUser.name}`}</li>
+                  </Link>
+                  <Link to={"/profile"}>
+                    <li>Profile</li>
+                  </Link>
+                </>
+              ) : (
+                <>
+                  <Link to={"/signin"}>
+                    <li>Sign In</li>
+                  </Link>
+                  <Link to={"/signup"}>
+                    <li>Sign Up</li>
+                  </Link>
+                </>
+              )}
             </ul>
           </div>
         </nav>
